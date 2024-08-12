bf45290347c77c121765605ac597c4ce77599f50
Quinn Semele <quinn@compasses.dev>
2024-08-12 20:06:55 +0100

    Remove quilt repos from qsl as they should be included by quilt loom.

--- quilt/build.gradle.kts
+++ quilt/build.gradle.kts
@@ -9,11 +9,6 @@ multiloader {
         create("qsl") {
             required()
 
-            requiresRepo("QuiltMC Maven", "https://maven.quiltmc.org/repository/release/", setOf(
-                "org.quiltmc",
-                "org.quiltmc.quilted-fabric-api"
-            ))
-
             artifacts {
                 modImplementation(group = "org.quiltmc", name = "quilt-loader", version = Constants.QUILT_LOADER_VERSION)
                 modImplementation(group = "org.quiltmc.quilted-fabric-api", name = "quilted-fabric-api", version = Constants.QUILT_API_VERSION)
